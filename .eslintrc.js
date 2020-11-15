397815bd003d49292d279f66a83f82a4872a9232
Wiwat Srisattha <wiwat_srt@hotmail.com>
2020-11-15 22:18:51 +0700

    style: add editor config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': ['error', 'never'],
     'comma-dangle': ['error', 'only-multiline'],
     'prettier/prettier': [
       'error',
