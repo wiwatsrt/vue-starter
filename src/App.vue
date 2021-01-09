f54682ebde9dc15982ee0205d73a86a7a2326b72
Wiwat Srisattha <wiwat_srt@hotmail.com>
2021-01-09 22:42:26 +0700

    feat: upgrade Tailwindcss 2.0

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="min-h-screen flex items-center justify-center">
     <h1 class="text-5xl font-bold text-purple-500">
-      Hello Vue 3 + TypeScript + Tailwind
+      Hello Vue 3 + TypeScript + Tailwind 2.0
     </h1>
   </div>
 </template>
